2599a5afd3e7fd324a0e642a4704dc676e2cb2e7
Pramith <aravindapramith3@gmail.com>
2019-06-04 12:33:32 +0530

    initializing

---
+++ src/main/java/lk/ijse/pos/dao/CrudDAO.java
@@ -0,0 +1,20 @@
+package lk.ijse.pos.dao;
+
+import lk.ijse.pos.entity.SuperEntity;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public interface CrudDAO<T extends SuperEntity, ID> extends SuperDAO {
+
+    void save(T entity) throws Exception;
+
+    void update(T entity) throws Exception;
+
+    void delete(ID entityId) throws Exception;
+
+    List<T> findAll() throws Exception;
+
+    T find(ID entityId) throws Exception;
+
+}
